12f93bc6fca96fc65a61c41e3f0c1946d443f2d4
floppyears <cedenoj@onid.oregonstate.edu>
2016-01-04 19:50:32 -0800

    Return single location in JSON API format
    CO-296

---
+++ src/main/groovy/edu/oregonstate/mist/locations/frontend/LocationConfiguration.groovy
@@ -0,0 +1,27 @@
+package edu.oregonstate.mist.locations.frontend
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import edu.oregonstate.mist.api.Credentials
+import io.dropwizard.Configuration
+import io.dropwizard.client.JerseyClientConfiguration
+
+import javax.validation.Valid
+import javax.validation.constraints.NotNull
+
+class LocationConfiguration extends Configuration {
+    @JsonProperty('authentication')
+    @NotNull
+    @Valid
+    List<Credentials> credentialsList
+
+    @JsonProperty('locations')
+    @NotNull
+    @Valid
+    Map<String, String> locationsConfiguration
+
+    @Valid
+    @NotNull
+    @JsonProperty("jerseyClient")
+    JerseyClientConfiguration jerseyClientConfiguration = new JerseyClientConfiguration()
+
+}
