77c8e2942afc7145e6ff35735528c645f6ae2312
floppyears <cedenoj@onid.oregonstate.edu>
2016-01-07 17:53:28 -0800

    Keep configuration class DRY
    CO-296

--- src/main/groovy/edu/oregonstate/mist/locations/frontend/LocationsFrontendConfiguration.groovy
+++ src/main/groovy/edu/oregonstate/mist/locations/frontend/LocationsFrontendConfiguration.groovy
@@ -1,19 +1,13 @@
 package edu.oregonstate.mist.locations.frontend
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import edu.oregonstate.mist.api.Credentials
-import io.dropwizard.Configuration
+import edu.oregonstate.mist.api.Configuration
 import io.dropwizard.client.JerseyClientConfiguration
 
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
 class LocationsFrontendConfiguration extends Configuration {
-    @JsonProperty('authentication')
-    @NotNull
-    @Valid
-    List<Credentials> credentialsList
-
     @JsonProperty('locations')
     @NotNull
     @Valid
