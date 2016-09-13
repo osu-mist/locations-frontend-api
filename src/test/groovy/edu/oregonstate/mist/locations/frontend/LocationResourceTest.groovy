95243c614c84afd93e469cc61a26c921df84d0a2
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-13 15:25:17 -0700

    MocFor testing

--- src/test/groovy/edu/oregonstate/mist/locations/frontend/LocationResourceTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/locations/frontend/LocationResourceTest.groovy
@@ -2,22 +2,15 @@ package edu.oregonstate.mist.locations.frontend
 
 import edu.oregonstate.mist.locations.frontend.db.LocationDAO
 import edu.oregonstate.mist.locations.frontend.resources.LocationResource
-import io.dropwizard.testing.junit.ResourceTestRule
-import org.junit.ClassRule
+import groovy.mock.interceptor.Mockfor
 import org.junit.Test
-import static org.mockito.Mockito.*
 
 class LocationResourceTest {
-
-    private static final LocationDAO locationDAO = mock(LocationDAO.class)
-
-    @ClassRule
-    public static final ResourceTestRule locationResource = ResourceTestRule.builder()
-            .addResource(new LocationResource(locationDAO))
-            .build()
-
     @Test
     public void testSanitize() {
+        def mock = new MockFor(LocationDAO)
+
+
         assert LocationResource.sanitize("Valley[!#]Library") == "Valley    Library"
         assert !LocationResource.sanitize(null)
     }
