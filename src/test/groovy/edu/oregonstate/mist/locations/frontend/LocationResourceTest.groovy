999e13ca3745a2da6d20d55e350f7d367f1745f1
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-13 10:43:23 -0700

    Initialize LocationResourceTest

---
+++ src/test/groovy/edu/oregonstate/mist/locations/frontend/LocationResourceTest.groovy
@@ -0,0 +1,18 @@
+package edu.oregonstate.mist.locations.frontend
+
+import edu.oregonstate.mist.locations.frontend.db.LocationDAO
+import edu.oregonstate.mist.locations.frontend.resources.LocationResource
+import io.dropwizard.testing.junit.ResourceTestRule
+import org.junit.ClassRule
+import org.junit.Test
+import static org.mockito.Mockito.*
+
+class LocationResourceTest {
+
+    private static final LocationDAO locationDAO = mock(LocationDAO.class)
+
+    @ClassRule
+    public static final ResourceTestRule locationResource = ResourceTestRule.builder()
+            .addResource(new LocationResource(locationDAO))
+            .build()
+}
