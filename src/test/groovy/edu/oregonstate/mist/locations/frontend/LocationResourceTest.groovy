74cfec14a45e98b002665384a33310ca4a30d36b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-14 11:01:56 -0700

    Implement testNoResults base on CO-363

--- src/test/groovy/edu/oregonstate/mist/locations/frontend/LocationResourceTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/locations/frontend/LocationResourceTest.groovy
@@ -1,16 +1,36 @@
 package edu.oregonstate.mist.locations.frontend
 
+import edu.oregonstate.mist.api.AuthenticatedUser
 import edu.oregonstate.mist.locations.frontend.db.LocationDAO
 import edu.oregonstate.mist.locations.frontend.resources.LocationResource
 import groovy.mock.interceptor.MockFor
 import org.junit.Test
 
 class LocationResourceTest {
+    static def user = new AuthenticatedUser('nobody')
+
+    @Test
+    public void testNoResults() {
+        def mock = new MockFor(LocationDAO)
+        mock.demand.search() {
+            String q, String campus, String type, Integer pageNumber, Integer pageSize ->
+                '{"hits": {"total": 0, "hits": []}}'
+        }
+
+        def dao = mock.proxyInstance()
+        def resource = new LocationResource(dao)
+        resource.uriInfo = new MockUriInfo()
+
+        def response = resource.list('dixon', null, null, user)
+        assert response.status == 200
+        assert response.entity.links == [:]
+        assert response.entity.data == []
+
+        mock.verify(dao)
+    }
+
     @Test
     public void testSanitize() {
-        def mock = new MockFor(LocationDAO)
-
-
         assert LocationResource.sanitize("Valley[!#]Library") == "Valley    Library"
         assert !LocationResource.sanitize(null)
     }
